106a8d741557a2ba80aa8c6f59fe7e805c1aa0d8
jcabme85@gmail.com Cabrero <jcabme85@gmail.com>
2018-03-22 17:15:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var name = "Susan"
-var height = "70"
+var height = "74"
 
 
 if (name === "Susan") {
@@ -7,3 +7,5 @@ if (name === "Susan") {
 } else {
     console.log("Expected the name to be Susan")
 }
+if (height === "74") {
+    console.log("The height is correct")
